6f7c320fd3d376ec0a63423268cdb28f321cf3c0
yyankov <yavor.yankov@lom.com>
2023-06-23 15:42:35 +0100

    Added second font; - implemented containers with icons and text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
 
@@ -7,6 +7,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final String sourceSans3Font = 'Source Sans 3';
+
   const MyApp({super.key});
 
   // This widget is the root of your application.
@@ -21,7 +23,7 @@ class MyApp extends StatelessWidget {
             children: <Widget>[
               // avatar
               CircleAvatar(
-                radius: 50,
+                radius: 100,
                 foregroundImage: AssetImage('img/avatar.png'),
               ),
               // name txt
@@ -33,6 +35,68 @@ class MyApp extends StatelessWidget {
                   color: Colors.white,
                 ),
               ),
+              // job title
+              Text(
+                "Software Developer",
+                style: TextStyle(
+                    fontFamily: sourceSans3Font,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 2),
+              ),
+              // phone number
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                padding: EdgeInsets.all(10),
+                child: Row(children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    '+44 123 456 789',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: sourceSans3Font,
+                      fontSize: 20,
+                    ),
+                  ),
+                ]),
+              ),
+              // email address
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                padding: EdgeInsets.all(10),
+                child: Row(children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'test.email@your.domain.com',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: sourceSans3Font,
+                      fontSize: 20,
+                    ),
+                  ),
+                ]),
+              )
             ],
           ),
         ),
