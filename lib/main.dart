c45f4fd4716f3cfc88659637065f854e2ae0c56b
yyankov <yavor.yankov@lom.com>
2023-06-23 13:26:02 +0100

    Init project and display avatar

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mi Card',
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                foregroundImage: AssetImage('img/avatar.png'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
